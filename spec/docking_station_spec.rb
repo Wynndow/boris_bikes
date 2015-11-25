9ed4af098b9ed960a02ba14c216c16cb29969e24
Chris Wynne <chris.wynndow@gmail.com>
2015-11-25 16:22:15 +0000

    Updates test to use :: for capacity constant

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,6 @@ require 'DockingStation'
 
 describe DockingStation do
 
-    #DEFAULT_CAPACITY = 20
-
     describe '#release_bike' do
           it { is_expected.to respond_to :release_bike }
 
@@ -26,8 +24,7 @@ describe DockingStation do
         end
 
         it 'raises an error if the dock is full' do
-          DEFAULT_CAPACITY = 20
-          DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
+          DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
           expect {subject.dock(Bike.new)}.to raise_error 'Dock is already full'
 
         end
