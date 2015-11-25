f5416b05c39e19eb3416ea033353dba2b3460f7d
Tony Young <ty_training@hotmail.co.uk>
2015-11-25 15:56:33 +0000

    private single responsibility methods

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -9,13 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available!' if @bikes.empty?
+    fail 'No bikes available!' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Dock is already full' if @bikes.length >= 20
+    fail 'Dock is already full' if full?
     @bikes << bike
+  end
+
+private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.count >= 20
   end
 
 end
